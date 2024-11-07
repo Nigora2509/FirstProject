cd2097818906a07c99ec63559420aec964902174
Nigora2509 <yarmukhamedovanigora@gmail.com>
2024-11-07 12:01:08 +0500

    code add

--- src/Program.cs
+++ src/Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            Console.Write("Sonni kiriting :");
+            var number = Convert.ToInt32(Console.ReadLine());
+            var thousand = number / 1000;
+            var hundred = number % 1000 / 100;
+            var tens = number % 1000 % 100 / 10;
+            var ones = number % 10;
+            var result = thousand + hundred + tens + ones;
+            Console.WriteLine(result);
         }
     }
 }
--- src/obj/Debug/net8.0/src.AssemblyInfo.cs
+++ src/obj/Debug/net8.0/src.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("src")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+db703491e9d3977334947b91aadf8f91af0a92fe")]
 [assembly: System.Reflection.AssemblyProductAttribute("src")]
 [assembly: System.Reflection.AssemblyTitleAttribute("src")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
